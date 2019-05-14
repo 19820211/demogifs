f36d28a5a4c3062f7a54e83e6c90c65554972e05
Grzegorz Stepien <Grzegorz>
2019-05-14 23:01:54 +0200

    Added a project to github

---
+++ src/main/java/com/ak/demogifs/DemogifsApplication.java
@@ -0,0 +1,13 @@
+package com.ak.demogifs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogifsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogifsApplication.class, args);
+	}
+
+}
