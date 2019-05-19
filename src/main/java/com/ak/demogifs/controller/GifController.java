16f1612aac67af65f8b991d0c0a1291475f56dd5
Grzegorz Stepien <Grzegorz>
2019-05-19 16:25:18 +0200

    Modification GifController and GifRepository

--- src/main/java/com/ak/demogifs/controller/GifController.java
+++ src/main/java/com/ak/demogifs/controller/GifController.java
@@ -1,5 +1,7 @@
 package com.ak.demogifs.controller;
 
+import com.ak.demogifs.model.repository.GifRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GifController {
 
+    @Autowired
+    GifRepository gifRepository;
+
     @RequestMapping("/showGifsInBrowser")
     @ResponseBody
     public String showGifsInBrowser(){
-        return "gifs";
+        return gifRepository.getGifNames();
     }
 
 
--- src/main/java/com/ak/demogifs/model/repository/GifRepository.java
+++ src/main/java/com/ak/demogifs/model/repository/GifRepository.java
@@ -1,10 +1,12 @@
 package com.ak.demogifs.model.repository;
 
 import com.ak.demogifs.model.Gif;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Repository
 public class GifRepository {
 
     private static List<Gif> ALL_GIFS = Arrays.asList(
@@ -15,4 +17,14 @@ public class GifRepository {
             new Gif("cowboy-coder", "code", false),
             new Gif("infinite-andrew", "andrew", false)
     );
+
+    //metoda która zapisuje wszystkie imiona do Stringa
+
+    public String getGifNames(){
+        String result = "";
+        for (Gif allGif : ALL_GIFS){
+            result += allGif.getName();
+        }
+        return result;
+    }
 }
